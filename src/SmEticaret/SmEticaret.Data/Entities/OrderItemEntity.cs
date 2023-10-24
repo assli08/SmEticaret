e6a21dbdb7517090c3310c5c5dd2069b1429ffd5
assli08 <assli.isildak@gmail.com>
2023-10-24 20:24:23 +0300

    Added data layer

---
+++ src/SmEticaret/SmEticaret.Data/Entities/OrderItemEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmEticaret.Data.Entities
+{
+    internal class OrderItemEntity : EntityBase
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public decimal Paid { get; set; }
+    }
+}
